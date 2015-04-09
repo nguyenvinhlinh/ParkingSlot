5bdb7d4435785d0d209a3f898523b3bd3524d189
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-04-09 23:07:02 +0700

    finished

--- ParkingSlot.py
+++ ParkingSlot.py
@@ -1,10 +1,7 @@
-import signal, sys, ssl, logging
+import signal, sys, ssl, logging,thread, range2
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
 from optparse import OptionParser 
-
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
-
-
 
 class ParkingSlot(WebSocket):
     p1 = True
@@ -15,7 +12,7 @@ class ParkingSlot(WebSocket):
         if self.data is None:
             self.data = ''
         try:
-            self.sendMessage('slot1:'+ str(self.p1)+',slot2:'+str(self.p2)+',slot3:'+str(self.p3)+',slot4:'+str(self.p4)) 
+            self.sendMessage('slot1:'+ str(range2.Output[0])+',slot2:'+ str(range2.Output[1])+',slot3:'+str(range2.Output[2])+',slot4:'+str(range2.Output[3])) 
         except Exception as n:
             print n
                 
@@ -54,6 +51,10 @@ class ParkingSlot(WebSocket):
         print "Hello"
 
 if __name__ == "__main__":
+    try:
+            thread.start_new_thread(range2.checkSensor, ())
+    except:
+        print 'unable to start new thread for sensor checker'
     cls = ParkingSlot
     server = SimpleWebSocketServer("localhost", 8000, cls)
 
