d22af8548635031b2d55bf8b9c7f5753d6158924
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-04-14 12:41:18 +0700

    remove unused libs

--- ParkingSlot.py
+++ ParkingSlot.py
@@ -1,13 +1,8 @@
 import signal, sys, ssl, logging,thread, range2
-from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer
-from optparse import OptionParser 
+from SimpleWebSocketServer import SimpleWebSocketServer
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 class ParkingSlot(WebSocket):
-    p1 = True
-    p2 = False
-    p3 = True
-    p4 = False
     def handleMessage(self):
         if self.data is None:
             self.data = ''
@@ -15,20 +10,7 @@ class ParkingSlot(WebSocket):
             self.sendMessage('slot1:'+ str(range2.Output[0])+',slot2:'+ str(range2.Output[1])+',slot3:'+str(range2.Output[2])+',slot4:'+str(range2.Output[3])) 
         except Exception as n:
             print n
-                
-        # for client in self.server.connections.itervalues():
-        #     if client != self:
-        #         try:
-        #             #client.sendMessage(str(self.address[0]) + ' - ' + str(self.data))
-        #             client.sendMessage(str(self.p1) + str(self.p2) + str(self.p3) + str(self.p4)  )
-        #         except Exception as n:
-        #             print n
-            # This will ignore the sending host
-            # else:
-        #     #     print "client is self"
             
-            
-
     def handleConnected(self):
         print self.address, 'connected'
         for client in self.server.connections.itervalues():
@@ -47,8 +29,6 @@ class ParkingSlot(WebSocket):
                 except Exception as n:
                     print n
 
-    def main():
-        print "Hello"
 
 if __name__ == "__main__":
     try:
@@ -61,6 +41,5 @@ if __name__ == "__main__":
     def close_sig_handler(signal, frame):
         server.close()
         sys.exit()
-
     signal.signal(signal.SIGINT, close_sig_handler)
     server.serveforever()
